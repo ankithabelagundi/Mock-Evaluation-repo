ce7550293ad35d47253f00e9ae7d3adc80af0834
Ankitha R B <ankithabelagundi@gmail.com>
2025-11-23 19:42:45 +0530

    Nested object  destructing

---
+++ Q5.js
@@ -0,0 +1,13 @@
+let user = {
+  name: "Alice",
+  address: {
+    city: "Bengaluru",
+    pin: 560001,
+    geo: { lat: 11.22, lng: 77.33 }
+  }
+};
+const{ name, address:{city, pin, geo} } =user;
+console.log(name);
+console.log(city);
+console.log(pin);
+console.log(geo);
